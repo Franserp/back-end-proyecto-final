b46bd35b134be13a60b9f763ac5844e132515d31
frank.dev <fran.contrerass2002@gmail.com>
2025-02-07 14:15:14 -0300

    solucion cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 6543;
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', "https://utn-proyecto-final-front-end-production.up.railway.app" ], 
+    origin: ['http://localhost:5173', "https://utn-proyecto-final-front-end-production.up.railway.app", "https://utn-proyecto-final-front-end.vercel.app" ], 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
